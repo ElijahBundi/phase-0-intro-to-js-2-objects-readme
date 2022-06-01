7f34c8ff6904a2f9d0768d4d619b6d95574a1f3c
Elijah <bundielly@gmail.com>
2022-06-01 22:23:04 +0300

    Good Lesson

---
+++ index.js
@@ -0,0 +1,117 @@
+/*
+const meals = {
+    breakfast: "Avocado toast",
+    lunch: "Avocado toast",
+    dinner: "Avocado toast",
+}
+  
+console.log(meals.breakfast)
+  // => "Avocado toast"
+  
+console.log(meals.dinner)
+  // => "Avocado toast"
+*/
+/*
+const meals = {
+    breakfast: "Avocado toast",
+    breakfast: "Oatmeal",
+    breakfast: "Scrambled eggs",
+}
+  
+console.log(meals)
+  // => { breakfast: "Scrambled eggs" }
+*/
+/*
+const street1 = "Dedan Road"
+const street2 = "2nd Dedan Close"
+const county = "Kajiado"
+const town = "Ngong"
+
+const address = {
+    street: {
+        street1: "Dedan Road",
+        street2: "2nd Dedan Close",
+    },
+    county: "Kajiado",
+    town: "Ngong",
+    zipCode: "00208"
+}
+console.log(address)
+console.log(address.street.street1)
+console.log(address.county)
+console.log(address.town)
+console.log(address.zipCode)
+
+address["street"];
+
+address["street"]["line1"];
+
+address["street"]["line2"];
+
+address["city"];
+
+address["state"];
+
+address["zipCode"];
+*/
+/*
+const meals = {
+    breakfast: "Oatmeal",
+    lunch: "Caesar salad",
+    dinner: "Chimichangas",
+}
+  
+let mealName = "lunch"
+  
+console.log(meals[mealName])
+  //=> "Caesar salad"
+*/
+
+const morningMeal = "breakfast";
+const middayMeal = "lunch";
+const eveningMeal = "dinner";
+
+const meals = {
+  [morningMeal]: "French toast",
+  [middayMeal]: "Personal pizza",
+  [eveningMeal]: "Fish and chips",
+};
+
+console.log(meals)
+console.log(Object.keys(meals))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
